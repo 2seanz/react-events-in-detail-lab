5cdff4639cddab439ac0de437d84444cae2766c0
Sean Ngo <seanzngo@gmail.com>
2021-04-28 10:26:47 -0400

    commit tree d9961c30df1fc89a1295a58f5e0b7a2477a4d2c5

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react' 
+
+
+export default class CoordinatesButton extends Component {
+
+    handleCoor = (e) => {
+        this.props.onReceiveCoordinates(e.clientX, e.clientY)
+    }
+
+
+    render() {
+        return (
+            <button onClick = {this.handleCoor}/>
+           
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,22 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react' 
+
+export default class DelayedButton extends Component {
+
+    handleBut = (e) => {
+        this.props.onDelayedClick
+        }
+
+    handleDelay = (e) => {
+        this.props.delay
+    }
+
+
+    render() {
+        return(
+            <button
+            onClick ={this.handleBut.setTimeout(this.handleDelay)}
+            ></button>
+        )
+    }
+}
